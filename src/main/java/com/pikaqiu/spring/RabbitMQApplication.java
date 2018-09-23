8a032871b1d0cc861d454e5f40a41290189e5c8a
pikaqiu <1047984367@qq.com>
2018-09-23 10:29:00 +0800

    第一次提交

--- src/main/java/com/pikaqiu/spring/RabbitMQApplication.java
+++ src/main/java/com/pikaqiu/spring/RabbitMQApplication.java
@@ -1,4 +1,4 @@
-package com.pikaqiu.demo;
+package com.pikaqiu.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/pikaqiu/spring/DemoApplicationTests.java
+++ src/test/java/com/pikaqiu/spring/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pikaqiu.demo;
+package com.pikaqiu.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
