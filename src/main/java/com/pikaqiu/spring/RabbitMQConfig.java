2ee03e9741c2eda49c0dd700f6256135b948811e
pikaqiu <1047984367@qq.com>
2018-09-24 16:46:04 +0800

    第一次提交

--- src/main/java/com/pikaqiu/spring/RabbitMQConfig.java
+++ src/main/java/com/pikaqiu/spring/RabbitMQConfig.java
@@ -1,9 +1,14 @@
 package com.pikaqiu.spring;
 
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -40,10 +45,83 @@ public class RabbitMQConfig {
 
         RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
 
+        //必须设置为true 不然spring容器不会加载
         rabbitAdmin.setAutoStartup(true);
 
         return rabbitAdmin;
 
+    }
+
+    /**
+     * 针对消费者配置
+     * 1. 设置交换机类型
+     * 2. 将队列绑定到交换机
+     * FanoutExchange: 将消息分发到所有的绑定队列，无routingkey的概念
+     * HeadersExchange ：通过添加属性key-value匹配
+     * DirectExchange:按照routingkey分发到指定队列
+     * TopicExchange:多关键字匹配
+     */
+
+    //第三种定义方式 直接在容器中创建bean 初始化加载的时候会根据类型去获取 exchange queue binding 然后进去初始化
+    @Bean
+    public TopicExchange exchange001() {
+        return new TopicExchange("topic001", true, false);
+    }
+
+    @Bean
+    public Queue queue001() {
+        return new Queue("queue001", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding001() {
+        return BindingBuilder.bind(queue001()).to(exchange001()).with("spring.*");
+    }
+
+    //队列二
+    @Bean
+    public TopicExchange exchange002() {
+        return new TopicExchange("topic002", true, false);
+    }
+
+    @Bean
+    public Queue queue002() {
+        return new Queue("queue002", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding002() {
+        return BindingBuilder.bind(queue002()).to(exchange002()).with("rabbit.*");
+    }
+
+
+    //队列三 和exchange001绑定关系  接受mq.*的路由key
+    @Bean
+    public Queue queue003() {
+        return new Queue("queue003", true); //队列持久
+    }
+
+    @Bean
+    public Binding binding003() {
+        return BindingBuilder.bind(queue003()).to(exchange001()).with("mq.*");
+    }
+
+    @Bean
+    public Queue queue_image() {
+        return new Queue("image_queue", true); //队列持久
+    }
+
+    @Bean
+    public Queue queue_pdf() {
+        return new Queue("pdf_queue", true); //队列持久
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+
+        return rabbitTemplate;
     }
 
 }
