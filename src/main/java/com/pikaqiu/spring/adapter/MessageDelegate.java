dfc4c2f075be006c80b60f525cb919ab1d3906c0
lixinyi <xinyi.li2@pactera.com>
2018-09-26 17:30:51 +0800

    测试代码

--- src/main/java/com/pikaqiu/spring/adapter/MessageDelegate.java
+++ src/main/java/com/pikaqiu/spring/adapter/MessageDelegate.java
@@ -19,10 +19,9 @@ public class MessageDelegate {
     public void handleMessage(byte[] messageBody) {
         System.err.println("默认方法, 消息内容:" + new String(messageBody));
     }
-	/*
 	public void consumeMessage(byte[] messageBody) {
 		System.err.println("字节数组方法, 消息内容:" + new String(messageBody));
-	}*/
+	}
 
 
     public void consumeMessage(String messageBody) {
